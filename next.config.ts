c71003a917880bbedbf28a552c6d7a0fd188e5b7
shagyshagy <64308831+shagyshagy@users.noreply.github.com>
2025-06-27 19:27:53 +0300

    Limit experimental dev origins to development environment

--- next.config.ts
+++ next.config.ts
@@ -18,10 +18,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  ...(process.env.NODE_ENV === 'development' && {
   experimental: {
     allowedDevOrigins: ['https://divine-bedbug-valued.ngrok-free.app'],
   },
-};
+}), // Only add this in development mode
+}
+
 
 
 export default nextConfig;
