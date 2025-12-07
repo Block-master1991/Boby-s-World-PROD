df9ba79d0da7632d3a4eb818015e781d6fa86a1c
shagyshagy <64308831+shagyshagy@users.noreply.github.com>
2025-12-07 19:53:51 +0300

    chore: update build script to use webpack and add .npmrc for legacy peer dependencies

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-   turbopack: {},
   images: {
     remotePatterns: [
       {
@@ -18,13 +17,31 @@ const nextConfig: NextConfig = {
   },
   
  // Only add this in development mode if NEXT_PUBLIC_DEV_ORIGIN is set
-  webpack: (config, { isServer }) => {
+    webpack: (config, { isServer }) => {
     // Add a rule to handle JSON files properly
     config.module.rules.push({
       test: /\.json$/,
       use: 'json-loader',
       type: 'javascript/auto',
     });
+
+    const pinoAliases = !isServer ? {
+        'pino': 'pino/browser',
+        'pino-pretty': 'pino-pretty/lib/browser',
+        'sonic-boom': false,
+        'thread-stream': false,
+    } : {};
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...pinoAliases,
+      'fastbench': false,
+      'pino-elasticsearch': false,
+      'tap': false,
+      'tape': false,
+      'desm': false,
+      'why-is-node-running': false,
+    };
 
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
